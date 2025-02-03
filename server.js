122b859b48cace735f328a67694d1943a6c123a9
zima563 <mohamedabdelazim654@gmail.com>
2025-02-03 14:56:28 +0200

    handle url cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config()
 app.use("/", express.static("uploads/"));
 app.use(compression());
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: process.env.URL }))
 routes(app);
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
